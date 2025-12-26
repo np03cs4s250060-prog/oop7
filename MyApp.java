fc367ff5a6b77d86ed551ac69e70991d7e41dc0e
np03cs4s250060-prog <np03cs4s250060@heraldcollege.edu.np>
2025-12-26 12:31:52 +0545

    Added Main class and updated application files

--- MyApp.java
+++ MyApp.java
@@ -1,5 +1,7 @@
 package week7;
 
 public class MyApp {
-
+	public static void main(String[] args) {
+		System.out.println("Hello git...");
+	}
 }
--- login.java
+++ login.java
@@ -1,5 +1,7 @@
 package week7;
 
 public class login {
-
+	public static void main (String[] args) {
+		System.out.println("Login form");
+	}
 }
